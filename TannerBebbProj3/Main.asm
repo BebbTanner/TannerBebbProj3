461b1710b3374f1e2f20e68f40cddd118c4efecf
Tanner Bebb <17tbebb@gmail.com>
2024-03-21 10:10:04 -0700

    Add project files.

---
+++ TannerBebbProj3/Main.asm
@@ -0,0 +1,17 @@
+;Tanner Bebb ASM project 3
+;First MASM program
+
+.386
+.model flat,stdcall
+.stack 4096
+include irvine32.inc
+ExitProcess proto, dwExitCode:dword
+
+.data
+
+.code
+
+main proc 
+
+main endp
+end main
