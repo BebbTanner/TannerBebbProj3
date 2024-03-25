3d898b017775da6c5b8f1270081573caccf5141a
Tanner Bebb <17tbebb@gmail.com>
2024-03-25 08:37:55 -0700

    Added documentation to all of the parts.

--- TannerBebbProj3/Main.asm
+++ TannerBebbProj3/Main.asm
@@ -11,37 +11,37 @@ ExitProcess proto, dwExitCode:dword
 .data
 
 ;4.) Write code defines symbolic names for several string literals. Use each symbolic name in a variable definition.
-	myMessage EQU 'Hello, World!'						;
-	myVar db myMessage									;
+	myMessage EQU 'Hello, World!'						;Declared myMessage as an EQU type so that it can be reassigned to a symbol.
+	myVar db myMessage									;Declared myVar and assigned myMessage to it. 
 ;End 4.)
 
 ;2.) Write code that defines symbolic constants for all seven days of the week. Create an array variable that uses the symbols as initializers.
-	Sun = 0												;
-	Mon = 1												;
-	Tue = 2												;
-	Wed = 3												;
-	Thu = 4												;
-	Fri = 5												;
-	Sat = 6												;
+	Sun = 0												;This block is just giving number values to all 7 days of the week.
+	Mon = 1
+	Tue = 2
+	Wed = 3
+	Thu = 4
+	Fri = 5
+	Sat = 6
 
-	warray BYTE Sun, Mon, Tue, Wed, Thu, Fri, Sat		;
+	warray BYTE Sun, Mon, Tue, Wed, Thu, Fri, Sat		;This is the array that stores all of the values for the days of the week declared above.
 ;End 2.)
 
 ;3.) The program must contain a definition of each data type listed in Table 3.2 in Section 3.4 of the textbook.
 ;Initialize each variable to a value that is consistent with its data type.
 
-	var1 BYTE 9h										;
-	var2 SBYTE -20										;
-	var3 WORD 800										;
-	var4 SWORD -1600									;
-	var5 DWORD 12345678h								;
-	var6 SDWORD -12345678h								;
-	var7 FWORD 0										;
-	var8 QWORD 1234567887654321h						;
-	var9 TBYTE 1000000000123456789h						;
-	var10 REAL4 -3.14									;
-	var11 REAL8 -3.1E+100								;
-	var12 REAL10 -3.1E+100								;
+	var1 BYTE 9h										;This block is just using all of the different data types for this chapter and giving them values.
+	var2 SBYTE -20
+	var3 WORD 800
+	var4 SWORD -1600
+	var5 DWORD 12345678h
+	var6 SDWORD -12345678h
+	var7 FWORD 0
+	var8 QWORD 1234567887654321h
+	var9 TBYTE 1000000000123456789h
+	var10 REAL4 -3.14
+	var11 REAL8 -3.1E+100
+	var12 REAL10 -3.1E+100
 
 ;End 3.)
 
