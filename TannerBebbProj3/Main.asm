c198b5deeaeefca39e8757248bbf960270dbee54
Tanner Bebb <17tbebb@gmail.com>
2024-03-25 08:32:54 -0700

    Cleaned up some of the documentation.

--- TannerBebbProj3/Main.asm
+++ TannerBebbProj3/Main.asm
@@ -2,11 +2,6 @@
 ;First MASM program
 ;Initial setup test
 
-;1.) X = (A + B) - (C + D)
-;2.) Write code that defines symbolic constants for all seven days of the week. Create an array variable that uses the symbols as initializers
-;3.) The program must contain a definition of each data type listed in Table 3.2 in Section 3.4 of the textbook. Initialize each variable to a value that is consistent with its data type
-;4.) Write code defines symbolic names for several string literals. Use each symbolic name in a variable definition
-
 .386
 .model flat,stdcall
 .stack 4096
@@ -15,37 +10,38 @@ ExitProcess proto, dwExitCode:dword
 
 .data
 
-;Start 4.)
-	myMessage EQU 'Hello, World!'
-	var 1 BYTE myMessage
+;4.) Write code defines symbolic names for several string literals. Use each symbolic name in a variable definition.
+	myMessage EQU 'Hello, World!'						;
+	myVar db myMessage									;
 ;End 4.)
 
-;Start 2.)
-	Sun = 0
-	Mon = 1
-	Tue = 2
-	Wed = 3
-	Thu = 4
-	Fri = 5
-	Sat = 6
+;2.) Write code that defines symbolic constants for all seven days of the week. Create an array variable that uses the symbols as initializers.
+	Sun = 0												;
+	Mon = 1												;
+	Tue = 2												;
+	Wed = 3												;
+	Thu = 4												;
+	Fri = 5												;
+	Sat = 6												;
 
-	warray BYTE Sun, Mon, Tue, Wed, Thu, Fri, Sat
+	warray BYTE Sun, Mon, Tue, Wed, Thu, Fri, Sat		;
 ;End 2.)
 
-;Start 3.)
+;3.) The program must contain a definition of each data type listed in Table 3.2 in Section 3.4 of the textbook.
+;Initialize each variable to a value that is consistent with its data type.
 
-	var1 BYTE 9h
-	var2 SBYTE -20
-	var3 WORD 800
-	var4 SWORD -1600
-	var5 DWORD 12345678h
-	var6 SDWORD -12345678h
-	var7 FWORD 0
-	var8 QWORD 1234567887654321h
-	var9 TBYTE 1000000000123456789h
-	var10 REAL4 -3.14
-	var11 REAL8 -3.1E+100
-	var12 REAL10 -3.1E+100
+	var1 BYTE 9h										;
+	var2 SBYTE -20										;
+	var3 WORD 800										;
+	var4 SWORD -1600									;
+	var5 DWORD 12345678h								;
+	var6 SDWORD -12345678h								;
+	var7 FWORD 0										;
+	var8 QWORD 1234567887654321h						;
+	var9 TBYTE 1000000000123456789h						;
+	var10 REAL4 -3.14									;
+	var11 REAL8 -3.1E+100								;
+	var12 REAL10 -3.1E+100								;
 
 ;End 3.)
 
@@ -53,7 +49,8 @@ ExitProcess proto, dwExitCode:dword
 
 main proc
 
-;Start 1.)
+;1.) Using the AddTwo program from Section 3.2 as a reference, write code to calculate the following expression, using registers: A = (A+B)-(C+D). 
+;Assign integer values to the EAX, EBX, ECX, and EDX registers
 	mov eax, 5											;eax = 5
 	mov ebx, 5											;ebx = 5
 	add eax, ebx										;(A + B)
